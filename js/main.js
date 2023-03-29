a9ea39e854f0a0e3b747fe1c80f04eb9801a8bad
BuildEnough <rhtmaehcl878@naver.com>
2023-03-29 11:27:54 +0900

    feat: countDown 기능

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 // 사용변수
 let score = 0;
+let time = 9;
+let isPlaying = false;
 
 const wordInput = document.querySelector('.word-input');
-const wordDisplay = document.querySelector('.word-display')
+const wordDisplay = document.querySelector('.word-display');
 const scoreDisplay = document.querySelector('.score');
+const timeDisplay = document.querySelector('.time');
 
-wordInput.addEventListener('input', ()=>{
+wordInput.addEventListener('input', () => {
   console.log(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase())
   if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
     score++;
@@ -13,3 +16,11 @@ wordInput.addEventListener('input', ()=>{
     wordInput.value = "";
   }
 })
+
+// 함수를 1초마다 실행시켜주는 인터벌
+setInterval(countDown, 1000);
+
+function countDown(){
+  time > 0 ? time-- : isPlaying = false;
+  timeDisplay.innerText = time;
+}
