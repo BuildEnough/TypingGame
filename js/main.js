7a7d2c346500ac4670b1d31cfb2ee75ea8ae8046
BuildEnough <rhtmaehcl878@naver.com>
2023-03-29 23:18:58 +0900

    feat: 렌더링 되었을때 체크, 단어 일치 체크 코드 변경

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let score = 0;
 let time = GAME_TIME;
 let isPlaying = false;
 let timeInterval;
+let words = [];
 
 const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
@@ -11,16 +12,33 @@ const scoreDisplay = document.querySelector('.score');
 const timeDisplay = document.querySelector('.time');
 const button = document.querySelector('.button');
 
-wordInput.addEventListener('input', () => {
-  console.log(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase())
-  if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
-    score++;
-    scoreDisplay.innerText = score;
-    wordInput.value = "";
-  }
-})
+// 렌더딩 되었을때 선언
+init();
 
-buttonChange('게임시작');
+function init() {
+  getWords();
+  wordInput.addEventListener('input', checkMatch)
+}
+
+// 단어 불러오기
+function getWords() {
+  words = ['Hello', 'Banana', 'Apple', 'Cherry'];
+  buttonChange('게임시작');
+}
+
+
+
+// 단어 일치 체크
+function checkMatch () {
+    console.log(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase())
+    if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+      score++;
+      scoreDisplay.innerText = score;
+      wordInput.value = "";
+    }
+  }
+
+
 
 function run() {
   isPlaying = true;
@@ -30,6 +48,7 @@ function run() {
 }
 
 function countDown() {
+  // 남은 시간 0초되면 isPlaying 가 false됨
   time > 0 ? time-- : isPlaying = false;
   if(!isPlaying) {
     clearInterval(timeInterval)
