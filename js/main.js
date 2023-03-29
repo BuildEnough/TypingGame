cf3d407c8d87fcf4bd86ee514a5b66795df6862b
BuildEnough <rhtmaehcl878@naver.com>
2023-03-29 11:18:27 +0900

    feat: 소문자 입력시 점수 1점 상승

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 // 사용변수
+let score = 0;
+
 const wordInput = document.querySelector('.word-input');
-console.log(wordInput)
+const wordDisplay = document.querySelector('.word-display')
+const scoreDisplay = document.querySelector('.score');
+
+wordInput.addEventListener('input', ()=>{
+  console.log(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase())
+  if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+    score++;
+    scoreDisplay.innerText = score;
+  }
+})
