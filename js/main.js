38d9276fc28e6ad7c3bb91ed49ad66f57ceaa049
BuildEnough <rhtmaehcl878@naver.com>
2023-03-29 22:08:33 +0900

    feat: 버튼 눌렀을때 게임 실행

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
 const scoreDisplay = document.querySelector('.score');
 const timeDisplay = document.querySelector('.time');
+const button = document.querySelector('.button');
 
 wordInput.addEventListener('input', () => {
   console.log(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase())
@@ -18,9 +19,18 @@ wordInput.addEventListener('input', () => {
 })
 
 // 함수를 1초마다 실행시켜주는 인터벌
-setInterval(countDown, 1000);
+//setInterval(countDown, 1000);
 
-function countDown(){
+buttonChange('게임시작');
+
+function countDown() {
   time > 0 ? time-- : isPlaying = false;
   timeDisplay.innerText = time;
+}
+
+
+
+function buttonChange(text) {
+  button.innerText = text;
+  text === '게임시작' ? button.classList.remove('loading') : button.classList.add('loading');
 }
