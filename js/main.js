003d10a63c24b826a98ecfeed37cc92d87d369d6
BuildEnough <rhtmaehcl878@naver.com>
2023-03-29 23:29:17 +0900

    feat: 게임 중이 아니면 점수 초기화

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let score = 0;
 let time = GAME_TIME;
 let isPlaying = false;
 let timeInterval;
+let checkInterval;
 let words = [];
 
 const wordInput = document.querySelector('.word-input');
@@ -20,6 +21,24 @@ function init() {
   wordInput.addEventListener('input', checkMatch)
 }
 
+// 게임 실행
+function run() {
+  isPlaying = true;
+  time = GAME_TIME;
+  // 함수를 1초마다 실행시켜주는 인터벌
+  timeInterval = setInterval(countDown, 1000);
+  checkInterval = setInterval(checkStatus, 50);
+}
+
+
+function checkStatus() {
+  if(!isPlaying && time === 0) {
+    buttonChange("게임 종료")
+    clearInterval(checkInterval)
+  }
+}
+
+
 // 단어 불러오기
 function getWords() {
   words = ['Hello', 'Banana', 'Apple', 'Cherry'];
@@ -27,25 +46,20 @@ function getWords() {
 }
 
 
-
 // 단어 일치 체크
 function checkMatch () {
-    console.log(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase())
+    // console.log(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase())
     if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+      wordInput.value = "";
+      // 게임중이아니면 점수 초기화
+      if(!isPlaying) {
+        return;
+      }
       score++;
       scoreDisplay.innerText = score;
-      wordInput.value = "";
     }
   }
 
-
-
-function run() {
-  isPlaying = true;
-  time = GAME_TIME;
-  // 함수를 1초마다 실행시켜주는 인터벌
-  timeInterval = setInterval(countDown, 1000);
-}
 
 function countDown() {
   // 남은 시간 0초되면 isPlaying 가 false됨
