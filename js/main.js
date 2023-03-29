7b580fca43f1c7e8caebe1e71e223f8672a60e91
BuildEnough <rhtmaehcl878@naver.com>
2023-03-29 22:51:04 +0900

    feat: 카운트다운 기능 추가, run 함수 추가

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 // 사용변수
 let score = 0;
-let time = 9;
+let time = 3;
 let isPlaying = false;
+let timeInterval;
 
 const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
@@ -18,10 +19,12 @@ wordInput.addEventListener('input', () => {
   }
 })
 
-// 함수를 1초마다 실행시켜주는 인터벌
-//setInterval(countDown, 1000);
-
 buttonChange('게임시작');
+
+function run() {
+  // 함수를 1초마다 실행시켜주는 인터벌
+  timeInterval = setInterval(countDown, 1000);
+}
 
 function countDown() {
   time > 0 ? time-- : isPlaying = false;
