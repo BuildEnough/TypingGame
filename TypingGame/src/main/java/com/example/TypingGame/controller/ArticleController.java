5ba9b430e65bfb9db7e385fa1eb87cfb9c67ae56
BuildEnough <rhtmaehcl878@naver.com>
2023-09-14 13:26:03 +0900

    데이터 조회하기

--- TypingGame/src/main/java/com/example/TypingGame/controller/ArticleController.java
+++ TypingGame/src/main/java/com/example/TypingGame/controller/ArticleController.java
@@ -6,14 +6,19 @@ import com.example.TypingGame.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -29,5 +34,15 @@ public class ArticleController {
         Article saved = articleRepository.save(article); // article 엔티티를 저장해 saved 객체에 반환
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+
+        return "articles/show";
     }
 }
