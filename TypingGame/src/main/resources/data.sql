e49faf0ef896727998f57d5988c923b8e5676975
BuildEnough <rhtmaehcl878@naver.com>
2023-09-15 12:01:12 +0900

    id 중복문제 해결

--- TypingGame/src/main/resources/data.sql
+++ TypingGame/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article(id, title, content) VALUES(1, 'qwer', '1111')
-INSERT INTO article(id, title, content) VALUES(2, 'asdf', '2222')
-INSERT INTO article(id, title, content) VALUES(3, 'zxcv', '3333')
+INSERT INTO article(title, content) VALUES('qwer', '1111')
+INSERT INTO article(title, content) VALUES('asdf', '2222')
+INSERT INTO article(title, content) VALUES('zxcv', '3333')
